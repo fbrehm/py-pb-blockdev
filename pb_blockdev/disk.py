0c62c55c5d2091ea8dc7b1379bf9026c5328d767
Frank Brehm <frank.brehm@profitbricks.com>
2013-05-22 22:19:01 +0200

    Added pb_blockdev/disk.py

---
+++ pb_blockdev/disk.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@organization: Profitbricks GmbH
+@copyright: © 2010 - 2013 by Profitbricks GmbH
+@license: GPL3
+@summary: Module for a class for partitioned Disk
+"""
+
+# Standard modules
+import sys
+import os
+import logging
+import re
+import glob
+import time
+
+# Third party modules
+
+# Own modules
+from pb_base.common import pp, to_unicode_or_bust, to_utf8_or_bust
+
+from pb_base.object import PbBaseObjectError
+from pb_base.object import PbBaseObject
+
+from pb_base.handler import PbBaseHandlerError
+from pb_base.handler import CommandNotFoundError
+from pb_base.handler import PbBaseHandler
+
+from pb_blockdev.base import BlockDeviceError
+from pb_blockdev.base import BlockDevice
+
+from pb_blockdev.translate import translator
+
+_ = translator.lgettext
+__ = translator.lngettext
+
+__version__ = '0.1.0'
+
+log = logging.getLogger(__name__)
+
+#---------------------------------------------
+# Some module variables
+
+
+#==============================================================================
+
+if __name__ == "__main__":
+
+    pass
+
+#==============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
