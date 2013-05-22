dc54067990025d9e030d1bd08ada8c2c78aa2ad4
Frank Brehm <frank.brehm@profitbricks.com>
2013-05-22 22:40:30 +0200

    Some changes on pb_blockdev/disk.py

--- pb_blockdev/disk.py
+++ pb_blockdev/disk.py
@@ -44,6 +44,86 @@ log = logging.getLogger(__name__)
 #---------------------------------------------
 # Some module variables
 
+VALID_DISK_UNIT_MODES = ('BYT', 'CHS', 'CYL')
+
+
+#==============================================================================
+class DiskError(BlockDeviceError):
+    """
+    Base error class for all exceptions belonging to the disk module
+    """
+
+    pass
+
+#==============================================================================
+class Disk(BlockDevice):
+    """
+    A class for encapsulating a partitioned disk (HD or such similar).
+    """
+
+    #--------------------------------------------------------------------------
+    def __init__(self,
+            name,
+            appname = None,
+            verbose = 0,
+            version = __version__,
+            base_dir = None,
+            use_stderr = False,
+            simulate = False,
+            *targs,
+            **kwargs
+        ):
+        """
+        Initialisation of the partitioned disk object.
+
+        @raise DiskError: on a uncoverable error.
+
+        @param name: name of the disk, e.g. 'sda'
+        @type name: None (if not even discoverd) or str
+        @param appname: name of the current running application
+        @type appname: str
+        @param verbose: verbose level
+        @type verbose: int
+        @param version: the version string of the current object or application
+        @type version: str
+        @param base_dir: the base directory of all operations
+        @type base_dir: str
+        @param use_stderr: a flag indicating, that on handle_error() the output
+                           should go to STDERR, even if logging has
+                           initialized logging handlers.
+        @type use_stderr: bool
+        @param simulate: don't execute actions, only display them
+        @type simulate: bool
+
+        @return: None
+
+        """
+
+        super(Disk, self).__init__(
+                name = name,
+                appname = appname,
+                verbose = verbose,
+                version = version,
+                base_dir = base_dir,
+                use_stderr = use_stderr,
+                simulate = simulate,
+                initialized = False,
+        )
+        self.initialized = False
+
+        self._disk_discovered = False
+        """
+        @ivar: flag, whether the disk was discovered via 'parted'
+        @type: bool
+        """
+
+        self.initialized = True
+
+    #------------------------------------------------------------
+    @property
+    def disk_discovered(self):
+        """A flag, whether the disk was discovered via 'parted'."""
+        return self._disk_discovered
 
 #==============================================================================
 
