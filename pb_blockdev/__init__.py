ce26700297f47d5e5401592e27984b1da62e2d37
Frank Brehm <frank.brehm@profitbricks.com>
2014-09-11 11:26:23 +0200

    Updated Copyright year in many files

--- pb_blockdev/__init__.py
+++ pb_blockdev/__init__.py
@@ -3,12 +3,12 @@
 """
 @author: Frank Brehm
 @contact: frank.brehm@profitbricks.com
-@copyright: © 2010 - 2013 by Frank Brehm, Berlin
+@copyright: © 2010 - 2014 by Frank Brehm, Berlin
 @summary: All modules for wrapper classes for different block devices
 """
 
 __author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
-__copyright__ = '(C) 2010-2013 by profitbricks.com'
+__copyright__ = '(C) 2010 - 2014 by profitbricks.com'
 __contact__ = 'frank.brehm@profitbricks.com'
 __version__ = '0.4.2'
 __license__ = 'GPL3'
