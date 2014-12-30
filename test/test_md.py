aa48dcd0382ef46367036832737f2b316e2294e6
Frank Brehm <frank.brehm@profitbricks.com>
2014-12-30 11:30:05 +0100

    Expanded test/test_md.py

--- test/test_md.py
+++ test/test_md.py
@@ -30,6 +30,8 @@ from general import BlockdevTestcase, get_arg_verbose, init_root_logger
 from pb_base.handler import CommandNotFoundError
 from pb_base.common import pp
 
+from pb_base.handler.lock import PbLock
+
 log = logging.getLogger(__name__)
 
 MDADM_PATH = os.sep + os.path.join('sbin', 'mdadm')
@@ -58,6 +60,9 @@ class MdTestcase(BlockdevTestcase):
 
         log.debug("Importing GenericMdHandler from  pb_blockdev.md ...")
         from pb_blockdev.md import GenericMdHandler
+
+        log.debug("Importing MdAdm from  pb_blockdev.md.admin ...")
+        from pb_blockdev.md.admin import MdAdm
 
     #--------------------------------------------------------------------------
     def test_transform_uuid(self):
@@ -101,6 +106,60 @@ class MdTestcase(BlockdevTestcase):
         if self.verbose > 2:
             log.debug("GenericMdHandler object:\n%s", pp(hdlr.as_dict(True)))
 
+    #--------------------------------------------------------------------------
+    @unittest.skipUnless(os.path.exists(MDADM_PATH), NOT_EXISTS_MSG)
+    def test_mdadm_lock(self):
+
+        log.info("Test global lock of a GenericMdHandler object ...")
+
+        import pb_blockdev.md
+        from pb_blockdev.md import GenericMdHandler
+
+        try:
+            hdlr = GenericMdHandler(verbose = self.verbose)
+        except CommandNotFoundError, e:
+            log.info(str(e))
+            return
+
+        hdlr.lock_global()
+        msg = "Object hdlr.global_lock is not a instance of PbLock."
+        self.assertIsInstance(hdlr.global_lock, PbLock, msg)
+        lockfile = hdlr.global_lock.lockfile
+        log.debug("Lockfile: %r.", lockfile)
+        if not os.path.exists(lockfile):
+            msg = "Lockfile %r does not exists."
+            self.fail(msg % (lockfile))
+        if self.verbose > 2:
+            log.debug("Global lock object:\n%s",
+                pp(hdlr.global_lock.as_dict(True)))
+
+        hdlr.global_lock = None
+        if os.path.exists(lockfile):
+            msg = "Lockfile %r still exists."
+            self.fail(msg % (lockfile))
+
+    #--------------------------------------------------------------------------
+    @unittest.skipUnless(os.path.exists(MDADM_PATH), NOT_EXISTS_MSG)
+    def test_mdadm_object(self):
+
+        log.info("Test init of a MdAdm object ...")
+
+        import pb_blockdev.md
+        import pb_blockdev.md.admin
+        from pb_blockdev.md.admin import MdAdm
+
+        try:
+            mdadm = MdAdm(verbose = self.verbose)
+        except CommandNotFoundError, e:
+            log.info(str(e))
+            return
+
+        if self.verbose > 1:
+            log.debug("repr of MdAdm object: %r", mdadm)
+
+        if self.verbose > 2:
+            log.debug("MdAdm object:\n%s", pp(mdadm.as_dict(True)))
+
 
 #==============================================================================
 
@@ -120,6 +179,8 @@ if __name__ == '__main__':
     suite.addTest(MdTestcase('test_import', verbose))
     suite.addTest(MdTestcase('test_transform_uuid', verbose))
     suite.addTest(MdTestcase('test_handler_object', verbose))
+    suite.addTest(MdTestcase('test_mdadm_lock', verbose))
+    suite.addTest(MdTestcase('test_mdadm_object', verbose))
 
     runner = unittest.TextTestRunner(verbosity = verbose)
 
