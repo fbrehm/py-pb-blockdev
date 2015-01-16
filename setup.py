015967df8102fa8184ee296905cf40a98f05d12d
Frank Brehm <frank.brehm@profitbricks.com>
2015-01-16 16:59:07 +0100

    Cleaned up setup.py for flake8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-from distutils.core import setup, Command
+from distutils.core import setup
 
 # own modules:
 cur_dir = os.getcwd()
@@ -20,30 +20,29 @@ import pb_blockdev
 
 packet_version = pb_blockdev.__version__
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = 'pb-blockdev',
-    version = packet_version,
-    description = 'ProfitBricks modules for wrapper classes of different block devices.',
-    long_description = read('README.txt'),
-    author = 'Frank Brehm',
-    author_email = 'frank.brehm@profitbricks.com',
-    url = 'ssh://git.profitbricks.localdomain/srv/git/python/pb-blockdev.git',
-    license = 'LGPLv3+',
-    platforms = ['posix'],
-    packages = [
+    name='pb-blockdev',
+    version=packet_version,
+    description='ProfitBricks modules for wrapper classes of different block devices.',
+    long_description=read('README.txt'),
+    author='Frank Brehm',
+    author_email='frank.brehm@profitbricks.com',
+    url='ssh://git.profitbricks.localdomain/srv/git/python/pb-blockdev.git',
+    license='LGPLv3+',
+    platforms=['posix'],
+    packages=[
         'pb_blockdev',
         'pb_blockdev.md',
         'pb_blockdev.megaraid',
         'pb_blockdev.multipath',
     ],
-    scripts = [
-#        'bin/megaraid-lds',
-#        'bin/megaraid-pds',
-    ],
-    classifiers = [
+    scripts=[],
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -54,14 +53,11 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    requires = [
+    requires=[
         'pb_base (>= 0.3.10)',
     ]
 )
 
-
-
-
-#========================================================================
+# =======================================================================
 
 # vim: fileencoding=utf-8 filetype=python ts=4 expandtab
